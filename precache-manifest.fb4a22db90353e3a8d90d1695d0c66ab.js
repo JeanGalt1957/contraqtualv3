5eb3900e43fa708ecc17537387870d9a793afa04
Matthew Gilbertson <matthew.f.gilbertson@gmail.com>
2022-04-10 01:43:43 -0400

    Updates

---
+++ precache-manifest.fb4a22db90353e3a8d90d1695d0c66ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "68d481a42f00fde86da6",
+    "url": "/contraqtualv3/static/js/main.68d481a4.chunk.js"
+  },
+  {
+    "revision": "f9b35c6fbc6ff12afdbd",
+    "url": "/contraqtualv3/static/js/1.f9b35c6f.chunk.js"
+  },
+  {
+    "revision": "1692d309c12e9f12453e",
+    "url": "/contraqtualv3/static/js/runtime~main.1692d309.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/contraqtualv3/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "d46849e695c8a415f7c71d4b21feadce",
+    "url": "/contraqtualv3/static/media/erc20.d46849e6.svg"
+  },
+  {
+    "revision": "5d1692b26ef29ea7b4e3722e1fc854b5",
+    "url": "/contraqtualv3/static/media/eth.5d1692b2.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/contraqtualv3/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "63db172409a52cb85373fb22e7f41958",
+    "url": "/contraqtualv3/index.html"
+  }
+];
